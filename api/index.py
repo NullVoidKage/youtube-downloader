4b76c4f32eaaec31d7a8365f4f50484d455af5f1
Nikko <nickouchiha5@gmail.com>
2023-06-17 21:21:48 +0800

    Modified Code

--- api/index.py
+++ api/index.py
@@ -1,11 +1,12 @@
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
-
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 from pytube import YouTube
+import os
 
 app = Flask(__name__)
+DOWNLOADS_DIR = 'downloads'  # Directory to store downloaded videos
 
 @app.route('/')
 def index():
@@ -18,14 +19,15 @@ def download():
         yt = YouTube(link)
         stream = yt.streams.get_highest_resolution()
         filename = f"{yt.title}.mp4"
-        stream.download(filename)
-        return redirect(url_for('success'))
+        filepath = os.path.join(DOWNLOADS_DIR, filename)
+        stream.download(output_path=DOWNLOADS_DIR, filename=filename)
+        return redirect(url_for('success', filename=filename))
     except Exception as e:
         return render_template('index.html', error=str(e))
 
-@app.route('/success')
-def success():
-    return render_template('success.html')
+@app.route('/success/<filename>')
+def success(filename):
+    return send_from_directory(DOWNLOADS_DIR, filename, as_attachment=True)
 
 if __name__ == '__main__':
     app.run()

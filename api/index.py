dbcde8df0371df92ee2b21a35193cabf15464fe7
Nikko <nickouchiha5@gmail.com>
2023-06-17 22:31:12 +0800

    added

--- api/index.py
+++ api/index.py
@@ -1,15 +1,11 @@
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask import Flask, render_template, request, redirect, url_for, send_file
 from pytube import YouTube
-import tempfile
-import os
-import dropbox
+import io
 
 app = Flask(__name__)
-DOWNLOADS_DIR = tempfile.mkdtemp()  # Create a temporary directory to store downloaded videos
-DROPBOX_ACCESS_TOKEN = 'sl.BgchpCKy5Ro2KyOVexj9sREL3Ae52a1v5LV1Xz2I8QHfg-PLexDy7q9RSvgWyKlJVwUIpaoVexVqAnesDRWUeAEMhkniF1PGOwlZnivbrG_eIx0oWfsT1eXKNbLvVItDgKEA87mYXP5s'  # Replace with your Dropbox access token
 
 @app.route('/')
 def index():
@@ -21,23 +17,17 @@ def download():
     try:
         yt = YouTube(link)
         stream = yt.streams.get_highest_resolution()
-        filename = f"{yt.title}.mp4"
-        filepath = os.path.join(DOWNLOADS_DIR, filename)
-        stream.download(output_path=DOWNLOADS_DIR, filename=filename)
+        buffer = io.BytesIO()
+        stream.stream_to_buffer(buffer)
+        buffer.seek(0)
 
-        # Upload the file to Dropbox
-        dbx = dropbox.Dropbox(DROPBOX_ACCESS_TOKEN)
-
-        with open(filepath, 'rb') as file:
-            dbx.files_upload(file.read(), f"/{filename}")
-
-        return redirect(url_for('success', filename=filename))
+        return send_file(
+            buffer,
+            attachment_filename=f"{yt.title}.mp4",
+            as_attachment=True
+        )
     except Exception as e:
         return render_template('index.html', error=str(e))
-
-@app.route('/success/<filename>')
-def success(filename):
-    return f"The video '{filename}' has been downloaded and uploaded to Dropbox successfully!"
 
 if __name__ == '__main__':
     app.run()

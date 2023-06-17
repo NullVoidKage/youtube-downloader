5c2847ed7287a29292218f55e5f5888f57561564
Nikko <nickouchiha5@gmail.com>
2023-06-17 20:14:13 +0800

    ADDED

--- api/index.py
+++ api/index.py
@@ -1,11 +1,31 @@
-from flask import Flask
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+
+from flask import Flask, render_template, request, redirect, url_for
+from pytube import YouTube
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/download', methods=['POST'])
+def download():
+    link = request.form['link']
+    try:
+        yt = YouTube(link)
+        stream = yt.streams.get_highest_resolution()
+        filename = f"{yt.title}.mp4"
+        stream.download(filename)
+        return redirect(url_for('success'))
+    except Exception as e:
+        return render_template('index.html', error=str(e))
+
+@app.route('/success')
+def success():
+    return render_template('success.html')
+
+if __name__ == '__main__':
+    app.run()

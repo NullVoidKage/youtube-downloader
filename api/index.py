0f1a95a6b343e72183744f4b5ed1dff57228b598
Nikko <nickouchiha5@gmail.com>
2023-06-17 20:43:32 +0800

    Modified Code

--- api/index.py
+++ api/index.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template, request, redirect, url_for, send_file
 from pytube import YouTube
 import tempfile
 import uuid
+import os
 
 app = Flask(__name__)
 
@@ -18,10 +19,11 @@ def download():
     try:
         yt = YouTube(link)
         stream = yt.streams.get_highest_resolution()
+        temp_dir = tempfile.mkdtemp()
         temp_filename = str(uuid.uuid4())
-        video_file = tempfile.NamedTemporaryFile(delete=False, prefix=temp_filename)
-        stream.download(output_path=video_file.name)
-        return send_file(video_file.name, as_attachment=True, attachment_filename=f"{yt.title}.mp4")
+        temp_filepath = os.path.join(temp_dir, temp_filename)
+        stream.download(output_path=temp_filepath)
+        return send_file(temp_filepath, as_attachment=True, attachment_filename=f"{yt.title}.mp4")
     except Exception as e:
         return render_template('index.html', error=str(e))
 

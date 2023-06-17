6980721f545d6309cf5df697729f021b35c058bb
Nikko <nickouchiha5@gmail.com>
2023-06-17 20:42:02 +0800

    Add

--- api/index.py
+++ api/index.py
@@ -4,6 +4,7 @@ ssl._create_default_https_context = ssl._create_unverified_context
 from flask import Flask, render_template, request, redirect, url_for, send_file
 from pytube import YouTube
 import tempfile
+import uuid
 
 app = Flask(__name__)
 
@@ -17,7 +18,8 @@ def download():
     try:
         yt = YouTube(link)
         stream = yt.streams.get_highest_resolution()
-        video_file = tempfile.NamedTemporaryFile(delete=False)
+        temp_filename = str(uuid.uuid4())
+        video_file = tempfile.NamedTemporaryFile(delete=False, prefix=temp_filename)
         stream.download(output_path=video_file.name)
         return send_file(video_file.name, as_attachment=True, attachment_filename=f"{yt.title}.mp4")
     except Exception as e:
